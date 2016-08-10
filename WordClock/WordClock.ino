5cc3b5f8ba4ecb92023b855e0b57d702984c81ef
Wingez <gustav.ledous@gmail.com>
2016-08-10 15:04:42 +0200

    Initial commit

---
+++ WordClock/WordClock.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		WordClock.ino
+ Created:	8/10/2016 2:55:51 PM
+ Author:	gustav
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
